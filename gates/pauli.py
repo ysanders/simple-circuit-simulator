dd48391aaceb7267bcc3966604ff20b500a91d24
Yuval Sanders <yuval.rishu.sanders@gmail.com>
2022-10-15 16:30:11 +1100

    added pauli gates

---
+++ gates/pauli.py
@@ -0,0 +1,15 @@
+def I:
+    return np.array([[1, 0],
+                     [0, 1]])
+
+def X:
+    return np.array([[0, 1],
+                     [1, 0]])
+
+def Y:
+    return np.array([[0, -1j], 
+                     [1j, 0]])
+
+def Z:
+    return np.array([[1, 0],
+                     [0, -1]])
