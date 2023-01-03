798a44e36c1c0e8df7c519445c35faca6e7a0276
Yuval Sanders <yuval.rishu.sanders@gmail.com>
2023-01-04 10:10:06 +1100

    added Hadamard gate

--- gates/gate_class.py
+++ gates/gate_class.py
@@ -27,5 +27,14 @@ class QuantumGate:
         return self.matrix
 
 def IdentityGate(num_qubits):
-    return QuantumGate(np.identity(2 ** num_qubits),
-                       name=f"IdentityGate({num_qubits})")
+    matrix = np.identity(2 ** num_qubits)
+    name = f"IdentityGate({num_qubits})"
+    return QuantumGate(matrix, name=name)
+
+def HadamardGate(diagonal_element, antidiagonal_element):
+    matrix = np.array([[diagonal_element,      antidiagonal_element],
+                       [antidiagonal_element, -1 * diagonal_element]])
+    matrix /= (diagonal_element**2 + antidiagonal_element**2)
+    assert matrix @ matrix == np.identity(2)
+    name = f"Hadamard({diagonal_element}, {antidiagonal_element})"
+    return QuantumGate(matrix, name=name)
